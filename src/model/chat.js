274a91a46b031cebca6dc3fefb91d6b71cc8fa3b
andrawan <andrawan.ep@gmail.com>
2021-01-26 02:55:55 +0700

    notif

--- src/model/chat.js
+++ src/model/chat.js
@@ -27,7 +27,7 @@ module.exports = {
   getRoomModel: (user_id) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        "SELECT room_id, user.user_id, user.user_email, user.user_name , user.user_image FROM room RIGHT JOIN user ON user.user_id = user_b WHERE user_a = ? AND status = 1",
+        "SELECT room_id, user.user_id, user.user_bio, user.user_name , user.user_image FROM room RIGHT JOIN user ON user.user_id = user_b WHERE user_a = ? AND status = 1",
         [user_id],
         (error, result) => {
           if (!error) {

fe5f529f13670e24296088a3519bb71ba089a244
andrawan <andrawan.ep@gmail.com>
2021-02-20 07:29:20 +0700

    Update chatweb_app.sql

--- chatweb_app.sql
+++ chatweb_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Feb 2021 pada 00.04
+-- Waktu pembuatan: 20 Feb 2021 pada 01.28
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.2.34
 
@@ -67,7 +67,16 @@ INSERT INTO `message` (`message_id`, `room_id`, `sender`, `receiver`, `message`,
 (168, 765151, 3, 1, '2', '2021-02-20 04:53:25'),
 (169, 765151, 1, 3, '3', '2021-02-20 04:53:27'),
 (170, 765151, 3, 1, '4', '2021-02-20 04:53:29'),
-(171, 765151, 3, 1, '123', '2021-02-20 04:53:47');
+(171, 765151, 3, 1, '123', '2021-02-20 04:53:47'),
+(172, 765151, 1, 3, 'hello there rudy', '2021-02-20 06:12:20'),
+(173, 765151, 1, 3, 'what are u up to?', '2021-02-20 06:12:26'),
+(174, 765151, 3, 1, 'hello andra', '2021-02-20 06:12:36'),
+(175, 765151, 3, 1, 'im doing homework', '2021-02-20 06:12:41'),
+(176, 765151, 1, 3, 'wow thats nice', '2021-02-20 06:12:49'),
+(177, 765151, 3, 1, 'thankyou', '2021-02-20 06:12:52'),
+(178, 765151, 3, 1, 'are you free tommorow?', '2021-02-20 06:13:40'),
+(179, 765151, 1, 3, 'yeah i am, whats up?????', '2021-02-20 06:13:56'),
+(180, 765151, 1, 3, 'asd', '2021-02-20 07:19:46');
 
 -- --------------------------------------------------------
 
@@ -103,11 +112,11 @@ CREATE TABLE `user` (
   `user_email` varchar(100) NOT NULL,
   `user_name` varchar(100) NOT NULL,
   `user_password` varchar(100) NOT NULL,
-  `user_image` varchar(100) NOT NULL,
-  `user_number` varchar(16) NOT NULL,
-  `user_lat` decimal(9,8) NOT NULL,
-  `user_lng` decimal(9,6) NOT NULL,
-  `user_bio` varchar(300) NOT NULL,
+  `user_image` varchar(100) DEFAULT NULL,
+  `user_number` varchar(16) DEFAULT NULL,
+  `user_lat` decimal(9,8) DEFAULT NULL,
+  `user_lng` decimal(9,6) DEFAULT NULL,
+  `user_bio` varchar(300) DEFAULT NULL,
   `user_created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `user_updated_at` datetime NOT NULL,
   `user_status` int(11) NOT NULL DEFAULT 1
@@ -118,12 +127,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_email`, `user_name`, `user_password`, `user_image`, `user_number`, `user_lat`, `user_lng`, `user_bio`, `user_created_at`, `user_updated_at`, `user_status`) VALUES
-(1, 'asd@asd', 'Rudy G', '$2b$10$mA4E9Q6EC84ouAHQY5AWEO7phM9EhmfyknlBkQsEWebhyuSi7iKqi', '2021-01-23T11-41-50.530Z3a389b0d-5b3f-4e25-99da-2da6f0f232d0.jpg', '088290056348', '-6.24332390', '107.060330', 'Hello there, i am a fullstack developer!', '2021-01-21 13:00:45', '2021-01-23 11:41:50', 1),
-(3, 'dsa@dsa', 'Vespa Matic', '$2b$10$mA4E9Q6EC84ouAHQY5AWEO7phM9EhmfyknlBkQsEWebhyuSi7iKqi', '2021-01-26T04-30-21.356Z08ede0fc-2fbe-4bf1-ac47-eb680a02c212.jpg', '16045', '-6.70666932', '107.595857', 'brem breeem', '2021-01-21 13:57:24', '2021-02-19 11:30:02', 1),
+(1, 'asd@asd', 'Rudy G', '$2b$10$mA4E9Q6EC84ouAHQY5AWEO7phM9EhmfyknlBkQsEWebhyuSi7iKqi', '2021-01-23T11-41-50.530Z3a389b0d-5b3f-4e25-99da-2da6f0f232d0.jpg', '088290056348', '-6.23999950', '107.065767', 'Hello there, i am a fullstack developer!', '2021-01-21 13:00:45', '2021-01-23 11:41:50', 1),
+(3, 'dsa@dsa', 'Vespa Matic', '$2b$10$mA4E9Q6EC84ouAHQY5AWEO7phM9EhmfyknlBkQsEWebhyuSi7iKqi', '2021-01-26T04-30-21.356Z08ede0fc-2fbe-4bf1-ac47-eb680a02c212.jpg', '16045', '-6.23999950', '107.065767', 'brem breeem', '2021-01-21 13:57:24', '2021-02-19 11:30:02', 1),
 (4, 'hehe@he', 'dasdasd', '$2b$10$mA4E9Q6EC84ouAHQY5AWEO7phM9EhmfyknlBkQsEWebhyuSi7iKqi', '', '', '0.00000000', '0.000000', '', '2021-01-21 13:57:39', '0000-00-00 00:00:00', 1),
 (5, 'andra@1', 'Andra 1', '$2b$10$JJZvHnd4QbCfXy.6f.28sO8mkoEhgwwP2B9RKiWlbGREP/MFZ72x2', '2021-01-23T17-08-10.588Z1600265022806_357490867_2.jpg', '1', '-6.23999950', '107.065767', 'i am andra one', '2021-01-23 17:06:27', '2021-01-23 17:08:10', 1),
 (6, 'andra@2', 'Andra 2', '$2b$10$GI3RLSjoHlmk3RQsAc.qRuxqZ0t/5a2htBGJ4R/kC0TWeAjf.J9WC', '2021-01-23T17-08-45.911Z51oW-Q2unWL._AC_SL1000_.jpg', '2', '-6.23999950', '107.065767', 'i am andra two', '2021-01-23 17:06:38', '2021-01-23 17:08:59', 1),
-(7, 'andra@4', 'Andra 3', '$2b$10$JXzRoQ/tZu6kMAaYXg6Yxu7OOdD9vEjF6JjIN0U2i/eZsaZPb6.gy', '', '', '0.00000000', '0.000000', '', '2021-01-23 17:06:45', '0000-00-00 00:00:00', 1);
+(7, 'andra@4', 'Andra 3', '$2b$10$JXzRoQ/tZu6kMAaYXg6Yxu7OOdD9vEjF6JjIN0U2i/eZsaZPb6.gy', '', '', '0.00000000', '0.000000', '', '2021-01-23 17:06:45', '0000-00-00 00:00:00', 1),
+(8, 'a@a', 'a', '$2b$10$k4Mc1kim0ET//LH.ab96DOJd2X/nSVrYbYMcMMWHM87yPtQ7iqx0e', '', '', '-6.23999950', '107.065767', '', '2021-02-20 00:27:15', '0000-00-00 00:00:00', 1);
 
 --
 -- Indexes for dumped tables
@@ -167,7 +177,7 @@ ALTER TABLE `friendlist`
 -- AUTO_INCREMENT untuk tabel `message`
 --
 ALTER TABLE `message`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
 
 --
 -- AUTO_INCREMENT untuk tabel `room`
@@ -179,7 +189,7 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
